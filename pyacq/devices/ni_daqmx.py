61c22e3743912c30ee2602236c36b5ca08966315
zoeith <zoeith@users.noreply.github.com>
2020-08-18 16:15:40 -0400

    streaming for nidaqmx from npbufferdevice

--- pyacq/devices/ni_daqmx.py
+++ pyacq/devices/ni_daqmx.py
@@ -30,12 +30,14 @@ if HAVE_NIDAQMX:
 
 class NIDAQmx(Node):
     """Simple wrapper around nidaqmx (official python wrapper of NI).
-    
+
     The main scenario is : continuous acquisition on AI channels giving an
-    pyacq output stream. While playing some short waveform on one or 
+    pyacq output stream. While playing some short waveform on one or
     several AO channels asynchronously.
-    
-    
+
+    This updated version uses buffered tasks streaming to continuously write
+    to AO channels asynchronously.
+
 
     Parameters for configure
     ----------
@@ -54,47 +56,65 @@ class NIDAQmx(Node):
         { 'Dev1/ai0': rse,  'Dev1/ai1':rse ...}
         If a str is given then it is applyed to all channels.
     chunksize : int
-        Number of samples to acquire per chunk per channel (1000 by default).
+        Number of samples to acquire per chunk per channel (1024 by default).
         Must compatible with internal fifo size of the device depending the nb
         of channel. If you have an error from nidaqmx like **To keep DMA or USB Bulk as...**
         change this value.
     magnitude_mode: str 'raw', 'float32_volt', 'float64_volt'
         Change the dtype of the output 'aichannels'
-          * 'raw' represent the int directly from the board. 
+          * 'raw' represent the int directly from the board.
           * 'float32_volt' and 'float64_volt' are the scale value in volts
-
+    streamao: bool (False)
+        If true, stream to ao from an input stream to this node.
+    aochunksize: int (optional | aocallback)
+        Specify the number of samples between calls to callback function.
+        Required if `aocallback` is specified or streamao.
 
     """
     _output_specs = {
         'aichannels': {}
+    }
+    _input_specs = {
+        'aochannels': {}
     }
 
     def __init__(self, **kargs):
         Node.__init__(self, **kargs)
         assert HAVE_NIDAQMX, "NIDAQmx node depends on the `nidaqmx` package, but it could not be imported."
 
-    def _configure(self, sample_rate=None,  chunksize=1000,
+    def _configure(self, sample_rate=None,  chunksize=1024,
             aichannels=[], airanges=(-5.,5.), aimodes='rse', magnitude_mode='raw',
-            aochannels=[]):
-        
+            aochannels=[], aoranges=(-5.,5.),
+            streamao=False, aochunksize=512, aosample_rate=None,
+            aicallback=None):
+
         self._chunksize = chunksize
+        self._aochunksize=aochunksize
+        self.streamao=streamao
+        self.aicallback = aicallback
+
         if type(airanges)!=dict and type(airanges)==tuple:
             airanges = {k:airanges for k in aichannels}
-        
+
         if type(aimodes)!=dict and type(aimodes)==str:
             aimodes = {k:aimodes for k in aichannels}
-        
+
+        if type(aoranges)!=dict and type(aoranges)==tuple:
+            aoranges = {k:aoranges for k in aochannels}
+
         self._nb_ai_channel = len(aichannels)
         self._nb_ao_channel = len(aochannels)
-        
+
         self._conf = {
             'aichannels': aichannels,
             'sample_rate': sample_rate,
             'airanges' : airanges,
             'aimodes' : aimodes,
             'aochannels' : aochannels,
+            'aoranges' : aoranges,
+            'aosample_rate': aosample_rate
         }
-        
+
         self._ai_dt = {'raw': 'int16',  'float32_volt':'float32',  'float64_volt':'float64'}[magnitude_mode]
         self.outputs['aichannels'].spec.update({
             'chunksize': chunksize,
@@ -103,7 +123,7 @@ class NIDAQmx(Node):
             'sample_rate': sample_rate,
             'nb_channel': len(aichannels),
         })
-        
+
         self.magnitude_mode = magnitude_mode
         if self.magnitude_mode!='raw':
             #TODO: change this when raw is not int16
@@ -113,10 +133,19 @@ class NIDAQmx(Node):
                 gain = (max_val-min_val)/2**16
                 self._ai_gains.append(gain)
             self._ai_gains = np.array(self._ai_gains, dtype=self._ai_dt).reshape(1, -1)
-        
+
+        if self.streamao:
+            self.inputs['aochannels'].spec.update({
+                'chunksize': aochunksize,
+                'shape' : (aochunksize, self._nb_ao_channel),
+                'dtype' : 'float64',
+                'sample_rate': aosample_rate,
+                'nb_channel':len(aochannels)
+            })
+
     def check_input_specs(self):
         pass
-    
+
     def check_output_specs(self):
         pass
 
@@ -126,11 +155,12 @@ class NIDAQmx(Node):
             self.outputs[outputname].params['channel_info'] = channel_info
 
     def _initialize(self):
-        sr = self._conf['sample_rate']
-        
+        sr = self._conf['sample_rate'] #this is ai sample rate
+
+        #initialize aitask
         if self._nb_ai_channel>0:
             aitask = nidaqmx.Task()
-            
+
             for chan in self._conf['aichannels']:
                 terminal_config = _ai_modes[self._conf['aimodes'].get(chan, 'nrse')]
                 min_val, max_val = self._conf['airanges'].get(chan, (-5., 5.))
@@ -140,45 +170,115 @@ class NIDAQmx(Node):
                             min_val=min_val, max_val=max_val,
                             units=const.VoltageUnits.VOLTS,
                             custom_scale_name="")
-            
+
             aiClockSource = ""
-            
+
             aitask.timing.cfg_samp_clk_timing(sr, source=aiClockSource, active_edge=const.Edge.RISING,
                             sample_mode= const.AcquisitionType.CONTINUOUS,
                             samps_per_chan=self._chunksize)
-            
+
             self.thread = DAQmxPollThread(self, parent=None)
         else:
             aitask = None
-        
+
+        # initialize aotask with backwards compatability for play_ao
+        if self.streamao:
+            aotask = nidaqmx.Task()
+            #ao sample rate which can be different than ai sample rate
+            aosr = self._conf['aosample_rate']
+            #this follows the pattern of play_ao
+            for chan in self._conf['aochannels']:
+                min_val, max_val = self._conf['aoranges'].get(chan, (-5., 5.))
+
+                aotask.ao_channels.add_ao_voltage_chan(chan,
+                            name_to_assign_to_channel="",
+                            min_val=min_val, max_val=max_val,
+                            units=const.VoltageUnits.VOLTS,
+                            custom_scale_name="")
+
+            if self._nb_ai_channel>0:
+                aoClockSource = "/Dev1/ai/SampleClock"
+            else:
+                aoClockSource = ""
+
+            #note here we have continuous playback using buffer and stream
+            #
+            aotask.timing.cfg_samp_clk_timing(aosr, source= "",
+                            active_edge=const.Edge.RISING,
+                            sample_mode=const.AcquisitionType.CONTINUOUS,
+                            samps_per_chan=self._aochunksize*2)
+
+
+        else:
+            aotask = None
+
         self.aitask = aitask
-        self.aotask = None
-    
+        self.aotask = aotask
+        # only make an aothread if there is an aotask
+        if self.aotask is not None:
+            self._aorunning = False
+            self.aopoller = ThreadPollInput(self.inputs['aochannels'], timeout=180,
+                                            return_data=True)
+            self.aopoller.new_data.connect(self._data_received)
+
+    def _data_received(self, ptr, data):
+        """
+        Why do we not need to check available space in daq buffer?
+        Lucky with the timer?
+        """
+        #it might be faster to preallocate buffer here
+        datanew = np.copy(data.T,order='C')
+        self.aotask.write(datanew, auto_start=False)
+        if not self._aorunning:
+            self.aotask.start()
+            self._aorunning = True
+
     def _start(self):
         self._n = 0
         if self.aitask is not None:
             self.aitask.start()
             self.thread.start()
+        if self.aotask is not None:
+            #self.aothread.start()
+            self.aopoller.start()
+            # must write to buffer before starting generation
+            # self.aotask.start()
 
     def _stop(self):
         if self.aitask is not None:
             self.thread.stop()
             self.thread.wait()
             self.aitask.stop()
+        if self.aotask is not None:
+            #self.aothread.stop()
+            #self.aothread.wait()
+            self.aopoller.stop()
+            self.aotask.write(np.zeros((self._nb_ao_channel,2)),auto_start=True)
+            self.aotask.stop()
+            self.aotask.close()
+
 
     def _close(self):
         if self.aitask is not None:
             self.aitask.close()
-    
+        if self.aotask is not None:
+            self.aotask.stop()
+            self.aotask.register_done_event(self._on_ao_done)
+            self.aotask.write(np.zeros((self._nb_ao_channel,2)),auto_start=True)
+        else:
+            self.play_ao(self._conf['aochannels'],
+                            np.zeros((self._nb_ao_channel,2)))
+
     def play_ao(self, aochannels, sigs):
         if self.aotask is not None:
             #one play is already running
             return
-        
-        sr = self._conf['sample_rate']
-        
+
+
+        sr = self._conf['aosample_rate']
+
         self.aotask = nidaqmx.Task()
-        
+
         for chan in aochannels:
             min_val, max_val = -10., 10.
             self.aotask.ao_channels.add_ao_voltage_chan(chan,
@@ -186,7 +286,7 @@ class NIDAQmx(Node):
                         min_val=min_val, max_val=max_val,
                         units=const.VoltageUnits.VOLTS,
                         custom_scale_name="")
-        
+
         if self._nb_ai_channel>0:
             aoClockSource = "/Dev1/ai/SampleClock"
         else:
@@ -200,7 +300,6 @@ class NIDAQmx(Node):
         self.aotask.write(sigs, auto_start=False)
         self.aotask.start()
 
-
     def _on_ao_done(self, task_handle, status, callback_data):
         self.aotask.close()
         self.aotask = None
@@ -213,32 +312,32 @@ class DAQmxPollThread(QtCore.QThread):
 
         self.lock = Mutex()
         self.running = False
-        
+
     def run(self):
         with self.lock:
             self.running = True
-        
+
         aitask = self.node.aitask
         chunksize = self.node._chunksize
         buffer_time = chunksize / self.node._conf['sample_rate']
         aitask.in_stream.timeout = timeout = buffer_time*10.
-        
+
         if self.node.magnitude_mode=='raw':
             raw_data = np.zeros((chunksize, self.node._nb_ai_channel), dtype='int16')
             raw_data_flat = raw_data
             raw_data_flat.reshape(-1)
         else:
             data_float64 = np.zeros((self.node._nb_ai_channel, chunksize), dtype='float64')
-        
-        
+
+
         stream = self.node.outputs['aichannels']
-        
+
         n = 0
         while True:
             with self.lock:
                 if not self.running:
                     break
-            
+
             if self.node.magnitude_mode=='raw':
                 nb_sample = aitask.in_stream.readinto(raw_data_flat)
                 if nb_sample==0:
@@ -253,11 +352,12 @@ class DAQmxPollThread(QtCore.QThread):
                 #~ scaled_data = data_float64.T.astype(self.node._ai_dt)
                 n += scaled_data.shape[0]
                 stream.send(scaled_data, index=n)
+            if self.node.aicallback is not None:
+                self.node.aicallback()
 
     def stop(self):
         with self.lock:
             self.running = False
-
 
 
 register_node_type(NIDAQmx)
--- pyacq/devices/npbufferdevice.py
+++ pyacq/devices/npbufferdevice.py
@@ -10,10 +10,10 @@ from pyqtgraph.Qt import QtCore, QtGui
 
 class NumpyDeviceBuffer(Node):
     """A fake analogsignal device.
-    
+
     This node streams data from a predefined buffer in an endless loop.
     """
-    _output_specs = {'signals': dict(streamtype='analogsignal', 
+    _output_specs = {'signals': dict(streamtype='analogsignal',
                                                 shape=(-1, 16), compression ='', sample_rate =30.
                                                 )}
 
@@ -33,22 +33,33 @@ class NumpyDeviceBuffer(Node):
             Length of chunks to send.
         buffer: array
             Data to send. Must have `buffer.shape[0] == nb_channel`.
+        cycle_times: int (Default:0)
+            Number of times to play the buffer. 0 for infinite.
+        cycled_callback: callback function (Default: None)
+            After sending cycle_times, optionally call this function.
+        cycle_end_set: float (Default: 0.0)
+            After cycle_times is reached, fill the buffer with this value.
         """
         return Node.configure(self, *args, **kwargs)
 
     def _configure(self, nb_channel=16, sample_interval=0.001, chunksize=256,
-                    buffer=None, channel_names=None):
+                    buffer=None, channel_names=None, cycle_times=0,
+                    cycled_callback=None, cycle_end_set = 0.0):
         self.nb_channel = nb_channel
         self.sample_interval = sample_interval
         self.chunksize = chunksize
-        
+        self.cycle_times = cycle_times
+        self.cycle_count = 0
+        self.cycled_callback =cycled_callback
+        self.cycle_end_set = cycle_end_set
+
         if channel_names is None:
             channel_names = [ 'chan{}'.format(c) for c in range(self.nb_channel) ]
         self.channel_names = channel_names
-        
+
         self.output.spec['shape'] = (-1, nb_channel)
         self.output.spec['sample_rate'] = 1. / sample_interval
-        
+
         if buffer is None:
             nloop = 40
             self.length = nloop * self.chunksize
@@ -58,12 +69,13 @@ class NumpyDeviceBuffer(Node):
             self.buffer = self.buffer.astype('float32')
         else:
             assert buffer.shape[1] == self.nb_channel, 'Wrong nb_channel'
-            assert buffer.shape[0] % chunksize == 0, 'Wrong buffer.shape[0] not multiple chunksize'
+            #see new version send_data
+            #assert buffer.shape[0] % chunksize == 0, 'Wrong buffer.shape[0] not multiple chunksize'
             self.buffer = buffer
             self.length = buffer.shape[0]
-        
+
         self.output.spec['dtype'] = buffer.dtype.name
-    
+
     def after_output_configure(self, outputname):
         if outputname == 'signals':
             channel_info = [ {'name': self.channel_names[c]} for c in range(self.nb_channel) ]
@@ -74,21 +86,54 @@ class NumpyDeviceBuffer(Node):
         ival = int(self.chunksize * self.sample_interval * 1000)
         self.timer = QtCore.QTimer(singleShot=False, interval=ival)
         self.timer.timeout.connect(self.send_data)
-    
+
     def _start(self):
         self.head = 0
         self.timer.start()
 
     def _stop(self):
         self.timer.stop()
-    
+
     def _close(self):
         pass
-    
-    def send_data(self):
-        i1 = self.head%self.length
-        self.head += self.chunksize
-        i2 = i1 + self.chunksize
-        self.output.send(self.buffer[i1:i2, :], index=self.head)
+
+    def reset(self):
+        """
+        send 0 to output and stop.
+        reset cycle_count to 0
+        """
+
+        super().stop()
+        self.cycle_count=0
+        data = np.ones((self.chunksize,self.nb_channel))*self.cycle_end_set
+        self.send_data(data=data)
+        if self.cycled_callback is not None:
+            self.cycled_callback()
+
+    def send_data(self,data=None):
+        if data is None:
+            i1 = self.head
+            i2 = i1 + self.chunksize
+            data=np.take(self.buffer,np.arange(i1,i2),mode='wrap',axis=0)
+            if i2 >= self.length:
+                self.head = np.remainder(i2,self.chunksize)
+                self.cycle_count += i2//self.length
+                #self.cycle_count += np.divmod(i2-i1,self.length)[0]
+            else:
+                self.head = i2
+            #check if now over number of cycle times
+            if (self.cycle_times>0) and (self.cycle_count>=self.cycle_times):
+                #make data just 1 cycle and fill with zeros
+                #call reset
+                times_over = self.cycle_count - self.cycle_times
+                final_values = self.cycle_end_set*np.ones(((times_over*self.length+self.head),self.nb_channel))
+                data[:-(times_over*self.length+self.head),:] = final_values
+                self.output.send(data,index=self.head)
+                self.reset() # stop things then send 0s.
+            else:
+                self.output.send(data,index=self.head)
+
+        else:
+            self.output.send(data,index=self.head)
 
 register_node_type(NumpyDeviceBuffer)
